28526023464380551d04e836144a1048790bdb6e
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-06-11 17:59:16 +0100

    added question

---
+++ app.rb
@@ -0,0 +1,11 @@
+puts "What setting do you want to use? a or b?"
+setting = gets
+
+puts "You chose setting " + setting
+
+puts "What numbers do you want to operate on?"
+number1 = gets
+number2 = gets
+
+puts "You chose " + number1
+puts "and " + number2
